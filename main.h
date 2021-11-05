744d08cf2dabcd020dd635c53ed8fd304058772c
Valentin_Tsushko <35625439+ValentinTsushko@users.noreply.github.com>
2021-11-05 19:40:10 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <algorithm>
+
+#pragma once
+
+using namespace std;
+
+#define m 10
+
+class Main
+{
+public:
+        int first_number();
+        int t();
+};
