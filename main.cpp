03812f476b72d329f9d3f8ec99d858e823a50184
Valentin_Tsushko <35625439+ValentinTsushko@users.noreply.github.com>
2021-11-05 19:38:26 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include "main_clases.cpp"
+
+bool compare(double a, double b )
+{
+        return (a > b);
+}
+
+int main()
+{
+        setlocale(LC_ALL, "Russian");
+        Var app;
+        short n;
+        cout << "Введите кол-во случайных чисел:  ";
+        cin >> n;
+
+        double x_0 = app.first_number();
+        double rand_numb[n];
+
+        for(int i = 0; i < n; i++)
+        {
+                //cout << "x_0 = " << x_0 << endl << endl;
+
+                int lambda = 8 * app.t() - 3;
+
+                double M = fmod((lambda * x_0),  m);
+
+                if(M < 0){
+                         M *= (-1);
+                }
+
+                //cout << "M = " << M << endl << endl;
+
+              //double x_1 = round((M / m) * 10) / 10; // округление до первого
+                double x_1 = M / m;
+                //cout << "x_1 = " << x_1 << endl << endl;
+                x_0 = x_1;
+                rand_numb[i] = x_1;
+        }
+        for(int i = 0; i < n; i++)
+        {
+                cout << endl << rand_numb[i];
+        }
+
+        sort(rand_numb, rand_numb + n, compare);
+
+/*
+        cout << endl << endl << endl;
+        for(int i = 0; i < n; i++)
+        {
+                cout << endl << rand_numb[i];
+        }
+*/
+        Pirson p;
+        cout << endl << "По критерию Пирсона эти числа: ";
+        p.Run(rand_numb) ? cout << "Равномерно распределены!" << endl : cout << "Неравномерно распределены" << endl;
+
+        //p.Run(rand_numb);
+
+        return 0;
+}
